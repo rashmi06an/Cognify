39d1c76e31c06d4e76b2bed9337f8259c11d8f3e
Vaageesh <Vaageesh@Vaageeshs-MacBook-Pro.local>
2025-11-28 03:01:26 +0530

    fixed server listen

--- cognify-backend/server.js
+++ cognify-backend/server.js
@@ -8,23 +8,22 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(cors({
-  origin: "*", 
+  origin: "*",
 }));
-
 
 app.use(express.json());
 
-
 app.use("/api/auth", authRoutes);
-
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
     );
   })
   .catch((err) => console.log(err));
